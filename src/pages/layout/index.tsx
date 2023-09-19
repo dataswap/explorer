44d89fea88ec1c33b072bfd8e5671060b49cbe87
waynewyang <yangwei.hit@gmail.com>
2023-09-19 09:24:20 +0800

    init

---
+++ src/pages/layout/index.tsx
@@ -0,0 +1,48 @@
+"use client"
+import { Breadcrumb, Layout, Menu, theme } from "antd"
+
+const { Header, Content, Footer } = Layout
+
+const App: React.FC = () => {
+    const {
+        token: { colorBgContainer },
+    } = theme.useToken()
+
+    return (
+        <Layout className="layout">
+            <Header style={{ display: "flex", alignItems: "center" }}>
+                <div className="demo-logo" />
+                <Menu
+                    theme="dark"
+                    mode="horizontal"
+                    defaultSelectedKeys={["2"]}
+                    items={new Array(3).fill(null).map((_, index) => {
+                        const key = index + 1
+                        return {
+                            key,
+                            label: `nav ${key}`,
+                        }
+                    })}
+                />
+            </Header>
+            <Content style={{ padding: "0 50px" }}>
+                <Breadcrumb style={{ margin: "16px 0" }}>
+                    <Breadcrumb.Item>Home</Breadcrumb.Item>
+                    <Breadcrumb.Item>List</Breadcrumb.Item>
+                    <Breadcrumb.Item>App</Breadcrumb.Item>
+                </Breadcrumb>
+                <div
+                    className="site-layout-content"
+                    style={{ background: colorBgContainer }}
+                >
+                    Content
+                </div>
+            </Content>
+            <Footer style={{ textAlign: "center" }}>
+                Dataswap ©2023 Created by Dataswap
+            </Footer>
+        </Layout>
+    )
+}
+
+export default App
