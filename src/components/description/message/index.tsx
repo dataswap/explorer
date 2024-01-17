edbc89c5eb2ac4faaefe6d9717e4f8015bc7ee39
waynewyang <yangwei.hit@gmail.com>
2024-01-17 09:29:37 +0800

    fix: 🐛 update msg desc

--- src/components/description/message/index.tsx
+++ src/components/description/message/index.tsx
@@ -1,31 +1,43 @@
 import React from "react"
+import { Descriptions } from "antd"
+import { ValueFields } from "@unipackage/utils"
+import {
+    DescriptionsItemTypeWithOptionalChildren,
+    convertDataToDescriptionsItems,
+} from "@unipackage/webkit"
 import Link from "next/link"
+
 import { DataswapMessage } from "@dataswapjs/dataswapjs"
-import { convertDataToItems } from "@unipackage/webkit"
 import {
     config_datasetDetailPageRoot,
     config_matchingDetailPageRoot,
 } from "../../../config/links"
-import { Descriptions } from "antd"
 
 interface IProps {
     data: DataswapMessage
 }
-function getMapper(data: DataswapMessage) {
+function generateSpecialItem(data: ValueFields<DataswapMessage>): {
+    [key in keyof DataswapMessage]?: DescriptionsItemTypeWithOptionalChildren
+} {
     return {
-        matchingId: (value: any) =>
-            value ? (
-                <Link href={`${config_matchingDetailPageRoot}/${value}`}>
-                    {value}
+        matchingId: {
+            children: (
+                <Link
+                    href={`${config_matchingDetailPageRoot}/${data.matchingId}`}
+                >
+                    {data.matchingId}
                 </Link>
-            ) : (
-                ""
             ),
-        datasetId: (value: any) => (
-            <Link href={`${config_datasetDetailPageRoot}/${value}`}>
-                {value}
-            </Link>
-        ),
+        },
+        datasetId: {
+            children: (
+                <Link
+                    href={`${config_datasetDetailPageRoot}/${data.datasetId}`}
+                >
+                    {data.datasetId}
+                </Link>
+            ),
+        },
     }
 }
 
@@ -36,10 +48,14 @@ export function MessageDescription({ data }: IProps) {
         cid: data.cid["/"],
         params: JSON.stringify(data.params),
     }
-    const descriptionItems = convertDataToItems(data, getMapper(data), {
-        //@ts-ignore
-        keyBlacklist: ["id", "_id", "__v"],
-    })
+    const descriptionItems = convertDataToDescriptionsItems(
+        data,
+        generateSpecialItem(data),
+        {
+            //@ts-ignore
+            keyBlacklist: ["id", "_id", "__v"],
+        }
+    )
     return (
         <Descriptions
             title="Message Info"
