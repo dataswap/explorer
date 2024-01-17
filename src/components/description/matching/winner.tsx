f544f4941f646d7cc975dd88588da69e2bdb6558
waynewyang <yangwei.hit@gmail.com>
2024-01-17 09:46:13 +0800

    fix: 🐛 update dataswapjs

--- src/components/description/matching/winner.tsx
+++ src/components/description/matching/winner.tsx
@@ -1,11 +1,12 @@
 import React from "react"
 import { Descriptions } from "antd"
 import { convertDataToDescriptionsItems } from "@unipackage/webkit"
-import { MatchingBid } from "../../../shared/types/index"
+import { MatchingBid } from "@dataswapjs/dataswapjs"
 
 interface IProps {
     data: MatchingBid
 }
+
 export function MatchingWinnerDescription({ data }: IProps) {
     const descriptionItems = convertDataToDescriptionsItems(data)
     return <Descriptions title="Winner Info" items={descriptionItems} />
--- src/shared/types/index.ts
+++
@@ -1,6 +0,0 @@
-//TODO: create this type in dataswapjs
-export interface MatchingBid {
-    bidder: string
-    amount: bigint
-    bidBlockNumer: bigint
-}
